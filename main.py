ff2ffdbe0d5a330b2011e953a5364197b7727b73
ViswajithJ <viswajith.j.official@gmail.com>
2022-12-03 20:44:10 +0530

    Implemented functions to get info on video link, display it, and download it. Basic exceptions handled.

--- main.py
+++ main.py
@@ -1,10 +1,80 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
+import pytube
+from pytube import YouTube
+from pytube import exceptions
+
+'''Function definitions'''
 
 
+# to get vid stream of highest res, info about video, return in list
+def get_vid_stream_data(link):
+    print("Inside get_vid_stream_data")
+    yt_obj = YouTube(link)
+    title = yt_obj.title
+    channel = yt_obj.author
+    length = round(yt_obj.length/60, 2)
+    videos = yt_obj.streams
+    vid_stream = videos.get_highest_resolution()
+    size = round(vid_stream.filesize*0.000001, 2)
+    res = vid_stream.__getattribute__("resolution")
+    yt_video_info = [vid_stream, title, channel, length, size, res]
+    print("got the video stream")
+    return yt_video_info
+
+
+def display_info(yt_video_info):
+    i = 0
+    rows = [
+        "Title           :   " + yt_video_info[1],
+        "Channel     :   " + yt_video_info[2],
+        "Duration    :   " + str(yt_video_info[3]) + "min",
+        "Size           :   " + str(yt_video_info[4]) + "MB",
+        "Resolution :    " + yt_video_info[5]
+    ]
+    for row in rows:
+        search_on_click = Label(
+            frame2, text=row, bg='black', fg='green', font=('calibri', 17))
+        search_on_click.place(relx=0, rely=i/8)
+        i += 1
+
+
+def download_video(vid_stream):
+    path = filedialog.askdirectory()
+    try:
+        vid_stream.download(path)
+        messagebox.showinfo("Youtube Video Downloader",
+                            "Downloaded successfully")
+    except:
+        messagebox.showerror("Youtube Video Downloader", "An error occured.")
+
+
+# on clicking search_btn
 def search_video():
-    print('hi')
+    link = search_entry.get()
+    if (link != ''):
+        try:
+            print("inside try")
+            yt_video_info = get_vid_stream_data(link)
+            display_info(yt_video_info)
+
+            download_btn = Button(
+                window, image=download_img, borderwidth=0, command=lambda: download_video(yt_video_info[0]))
+            download_btn.place(relx=0.35, rely=0.7,
+                               relheight=0.17, relwidth=0.30)
+        except exceptions.RegexMatchError:
+            messagebox.showerror("Youtube Video Downloader",
+                                 "Enter valid youtube video url")
+        except exceptions.VideoUnavailable:
+            messagebox.showerror("Youtube Video Downloader",
+                                 "Video is unavailable")
+        except Exception as err:
+            messagebox.showerror(
+                "Youtube Video Downloader", err)
+    else:
+        messagebox.showwarning("Youtube Video Downloader", "Enter a url")
+    print("hi")
 
 
 # main program
@@ -18,6 +88,7 @@ canvas.pack()
 bg_img = PhotoImage(file='./images/bg_image1.png')
 yt_image1 = PhotoImage(file="./images/youtube-icon72.png")
 search_img = PhotoImage(file="./images/search.png")
+download_img = PhotoImage(file="./images/download_img1.png")
 
 # background-img
 bg_label = Label(window, image=bg_img)
@@ -28,7 +99,7 @@ frame1 = Frame(window, bg='white', bd=5, borderwidth=0)
 frame1.place(relx=0.5, rely=0.05, relheight=0.12, relwidth=0.75, anchor='n')
 
 # frame for displaying Video-Data
-frame2 = Frame(window, bg='white', bd=5, borderwidth=0)
+frame2 = Frame(window, borderwidth=10, bg='black', bd=5, border=5)
 frame2.place(relx=0.5, rely=0.3, relheight=0.40, relwidth=0.75, anchor='n')
 
 
@@ -37,7 +108,7 @@ yt_label = Label(frame1, image=yt_image1, borderwidth=0)
 yt_label.place(relx=0, rely=0, relheight=1, relwidth=0.1)
 
 # placing Entry box in frame1
-search_entry = Entry(frame1, borderwidth=5, fg='black', font=('calibri', 17))
+search_entry = Entry(frame1, borderwidth=5, fg='black', font=('calibri', 15))
 search_entry.place(relx=0.1, rely=0, relheight=1, relwidth=0.8)
 search_entry.insert(END, "Replace with a valid youtube url")
 
